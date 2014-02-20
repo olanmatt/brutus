1aa6eb9f39b00c248155907fd627e0d2a59d93fc
Matt Olan <olanmatt@gmail.com>
2014-02-19 20:24:58 -0500

    Added usage message and version information

--- main.c
+++ main.c
@@ -3,6 +3,27 @@
 #include <getopt.h>
 #include "permute.h"
 
+void usage(){
+	printf("Usage: brutus [-w] [-c charset] [-m min] [-M max]\n");
+	printf("Generate string permutations of charset to standard output.\n\n");
+	printf("  -c, --charset\t\tcharset to permute over\n");
+	printf("  -m, --min\t\tminimum length of strings\n");
+	printf("  -M, --max\t\tmaximum length of strings\n");
+	printf("  -w, --no-replace\tdo not reuse letters from charset\n");
+	printf("      --help\t\tdisplay this help and exit\n");
+	printf("      --version\t\toutput version infomation and exit\n");
+}
+
+void version(){
+	printf("brutus 0.1\n");
+	printf("Copyright (C) 2014 Matt Olan.\n");
+	printf("License: The MIT License (MIT).\n\n");
+	printf("This is free software: you are free to change and redistribute it.\n");
+	printf("There is NO WARRANTY, to the extent permitted by law.\n\n");
+	printf("Written by Matt Olan.");
+
+}
+
 int main (int argc, char **argv){
 
 	int c;
@@ -13,24 +34,30 @@ int main (int argc, char **argv){
 
 	while (1){
 		static struct option long_options[] = {
-			/* These options don't set a flag.
-			 * We distinguish them by their indices.
-			 */
-			{"charset",	required_argument, 0, 'c'},
-			{"min",		required_argument, 0, 'm'},
-			{"max",		required_argument, 0, 'M'},
+			{"charset",	required_argument,	0, 'c'},
+			{"min",		required_argument,	0, 'm'},
+			{"max",		required_argument,	0, 'M'},
+			{"help",	no_argument,		0, 'h'},
+			{"version",	no_argument,		0, 'V'}, 
 			{0, 0, 0, 0}
 		};
-		/* getopt_long stores the option index here. */
+		
 		int option_index = 0;
+		c = getopt_long(argc, argv, "h:c:m:M:", long_options, &option_index);
 
-		c = getopt_long(argc, argv, "abc:dm:fM:", long_options, &option_index);
-
-		/* Detect the end of the options. */
 		if (c == -1)
 			break;
 
 		switch (c){
+
+			case 'h':
+				usage();
+				exit(0);
+
+			case 'V':
+				version();
+				exit(0);
+			
 			case 'c':
 				printf ("option -c with value `%s'\n", optarg);
 				break;
@@ -43,12 +70,9 @@ int main (int argc, char **argv){
 				max = atoi(optarg);
 				break;
 
-			case '?':
-				/* getopt_long already printed an error message. */
-				break;
-
 			default:
-				abort ();
+				printf("Try `cat --help' for more information.");
+				exit(1);
 		}
 	}
 
